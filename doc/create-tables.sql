49ae3cee9ab5645efd414b9bcdb6d1c5bf0af730
ququzone <xueping.yang@gmail.com>
2016-03-18 10:30:08 +0800

    init project

---
+++ doc/create-tables.sql
@@ -0,0 +1,42 @@
+CREATE TABLE users (
+  id           VARCHAR(36) NOT NULL,
+  username     VARCHAR(12) NOT NULL UNIQUE,
+  password     VARCHAR(50) NOT NULL,
+  status       VARCHAR(20) NOT NULL,
+  created_time TIMESTAMP   NOT NULL,
+  updated_time TIMESTAMP DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE resources (
+  id           VARCHAR(36)  NOT NULL,
+  name         VARCHAR(100) NOT NULL,
+  pattern      VARCHAR(100) NOT NULL UNIQUE,
+  method       VARCHAR(10)  NOT NULL,
+  status       VARCHAR(20)  NOT NULL,
+  type         VARCHAR(50)  NOT NULL,
+  created_time TIMESTAMP    NOT NULL,
+  updated_time TIMESTAMP DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+  id           VARCHAR(36)  NOT NULL,
+  name         VARCHAR(100) NOT NULL UNIQUE,
+  status       VARCHAR(20)  NOT NULL,
+  created_time TIMESTAMP    NOT NULL,
+  updated_time TIMESTAMP DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role_resources (
+  role_id       VARCHAR(36) NOT NULL REFERENCES roles (id),
+  permission_id VARCHAR(36) NOT NULL REFERENCES resources (id),
+  PRIMARY KEY (role_id, permission_id)
+);
+
+CREATE TABLE role_users (
+  role_id VARCHAR(36) NOT NULL REFERENCES roles (id),
+  user_id VARCHAR(36) NOT NULL REFERENCES users (id),
+  PRIMARY KEY (role_id, user_id)
+);
