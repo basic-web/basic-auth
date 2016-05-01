2445de41e9070f95aa047698d08797cacd0ac725
ququzone <xueping.yang@gmail.com>
2016-05-01 22:02:38 +0800

    add resources request mapping

--- src/main/java/com/github/ququzone/basicauth/web/ResourceController.java
+++ src/main/java/com/github/ququzone/basicauth/web/ResourceController.java
@@ -1,6 +1,10 @@
 package com.github.ququzone.basicauth.web;
 
+import com.github.ququzone.basicauth.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * resource controller.
@@ -9,4 +13,11 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class ResourceController {
+    @Autowired
+    private AuthService authService;
+
+    @RequestMapping(value = "/resources", method = RequestMethod.GET)
+    public String resources() {
+        return "auth/resources";
+    }
 }
