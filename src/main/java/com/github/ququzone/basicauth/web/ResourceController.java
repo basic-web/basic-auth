fca113ebd1b9ef2ab7f9425c58c9e549c88b3377
ququzone <xueping.yang@gmail.com>
2016-05-01 22:00:54 +0800

    add resource controller

---
+++ src/main/java/com/github/ququzone/basicauth/web/ResourceController.java
@@ -0,0 +1,12 @@
+package com.github.ququzone.basicauth.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * resource controller.
+ *
+ * @author Yang XuePing
+ */
+@Controller
+public class ResourceController {
+}
