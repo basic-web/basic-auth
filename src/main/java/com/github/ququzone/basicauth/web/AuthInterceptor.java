22873308d4976040a675e63a13702975ff4910b4
ququzone <xueping.yang@gmail.com>
2016-07-14 09:45:43 +0800

    auth interceptor check ajax request

--- src/main/java/com/github/ququzone/basicauth/web/AuthInterceptor.java
+++ src/main/java/com/github/ququzone/basicauth/web/AuthInterceptor.java
@@ -2,6 +2,7 @@ package com.github.ququzone.basicauth.web;
 
 import com.github.ququzone.basicauth.model.ResourceMapping;
 import com.github.ququzone.basicauth.service.AuthService;
+import com.github.ququzone.common.web.JsonResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,7 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestPath = request.getRequestURI().substring(
                 request.getContextPath().length());
+        boolean ajax = request.getHeader("X-Requested-With") != null && !request.getHeader("X-Requested-With").isEmpty();
         if (requestPath.endsWith(".jsp")) {
             return true;
         }
@@ -33,7 +35,12 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
             if (LOG.isInfoEnabled()) {
                 LOG.info("user not login for request:" + requestPath);
             }
-            response.sendRedirect("/login?next=" + requestPath);
+            response.setStatus(400);
+            if (ajax) {
+                response.getWriter().println(JsonResult.message("你尚未登录系统"));
+            } else {
+                response.sendRedirect("/login?next=" + requestPath);
+            }
             return false;
         }
         if (!authService.auditing(userId, requestPath,
@@ -41,7 +48,12 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
             if (LOG.isInfoEnabled()) {
                 LOG.info("user not permission for request:" + requestPath);
             }
-            response.sendRedirect("/no_permission?next=" + requestPath);
+            response.setStatus(400);
+            if (ajax) {
+                response.getWriter().println(JsonResult.message("没有权限访问"));
+            } else {
+                response.sendRedirect("/no_permission?next=" + requestPath);
+            }
             return false;
         }
         return true;
